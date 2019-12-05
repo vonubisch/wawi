f5d9df599f7f0a4dc3ceabefb0f58c827d32cd0c
Bjorn <bs@vonubisch.com>
2019-12-05 01:26:24 +0100

    observer

--- services/extension-chrome/src/background.js
+++ services/extension-chrome/src/background.js
@@ -5,3 +5,8 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 chrome.tabs.onUpdated.addListener(() => {
     console.log('Updated');
 });
+
+chrome.alarms.create({periodInMinutes: 1});
+chrome.alarms.onAlarm.addListener(function() {
+    console.log("Hello, world!", new Date())
+});
--- services/extension-chrome/src/content.js
+++ services/extension-chrome/src/content.js
@@ -1,4 +1,6 @@
 import $ from "jquery";
+import get from 'lodash/get';
+import last from 'lodash/last';
 
 import { test } from 'packages/core/interface';
 
@@ -50,8 +52,70 @@ $(document).ready(function() {
 
         online = currentStatus;
 
-        setFavicon( online ? (currentSeconds % 2 === 0) ? redFavicon : avatar : fallbackFavicon);
+        // setFavicon( online ? (currentSeconds % 2 === 0) ? redFavicon : avatar : fallbackFavicon);
 
     }, 500);
 
+});
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+    // fired when a mutation occurs
+    // console.log(mutations, observer);
+
+    if (get(mutations, [0, 'target', 'innerText'], '').length > 0) {
+        console.log('0.target.innerText', get(mutations, [0, 'target', 'innerText'], '').split('\n'), mutations);
+        if (get(get(mutations, [0, 'target', 'innerText'], '').split('\n'), [1], '').length > 0) {
+            console.log('0.target.innerText.split.1', get(get(mutations, [0, 'target', 'innerText'], '').split('\n'), [1], ''), mutations);
+        }
+    }
+
+    if (
+        last(get(mutations, [0, 'target', 'innerText'], '').split('\n')) === 'Type a message'
+    ) {
+        const contactName = get(get(mutations, [0, 'target', 'innerText'], '').split('\n'), [0], '');
+        const contactStatus = get(get(mutations, [0, 'target', 'innerText'], '').split('\n'), [1], '') === 'online';
+        console.log(`!! Switched chat to '${contactName}' who is ${contactStatus ? 'online' : 'offline'}`, new Date(), mutations);
+    }
+
+    if (
+        get(mutations, [0, 'type']) === 'characterData' &&
+        get(mutations, [0, 'target', 'data']) === 'online' &&
+        get(mutations, [1, 'type']) === 'attributes' &&
+        get(mutations, [1, 'oldValue']) === 'click here for contact info'
+    ) {
+        console.log('!! Contact online (initial)', new Date(), mutations);
+    }
+
+    if (
+        get(mutations, [0, 'target', 'innerText'], '').split('\n').length === 2 &&
+        get(get(mutations, [0, 'target', 'innerText'], '').split('\n'), [1], '') === 'online'
+    ) {
+        console.log('!! Contact online (idle)', new Date(), mutations);
+    }
+
+    if (
+        get(get(mutations, [0, 'target', 'innerText'], '').split('\n'), [1], '') === 'online' && 
+        last(get(mutations, [0, 'target', 'innerText'], '').split('\n')) === 'Type a message'
+    ) {
+        console.log('!! Contact online (switch)', new Date(), mutations);
+    }
+
+    mutations.forEach(mutation => {
+        console.log(mutation.target.title);
+    });
+
+    // ...
+});
+
+// define what element should be observed by the observer
+// and what types of mutations trigger the callback
+observer.observe(document, {
+    subtree: true,
+    childList: true,
+    characterData: true,
+    attributes: true,
+    attributeOldValue: true,
+    attributeFilter: ['title']
 });
